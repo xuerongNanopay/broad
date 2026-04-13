8daaea2416da6a05fd32198b092e5973c57f3aa4
xuerongNanopay <xuerong@nanopay.net>
2026-04-13 18:34:14 -0400

    initial cli module

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,4 +1,18 @@
 """CLI module for broad."""
+import typer
 
-def app():
-    print("CLI entry")
+app = typer.Typer(
+    name="broad",
+    # context_settings={"help_option_names": ["-h", "--help"]},
+    help="Broad - AI Robots.",
+    no_args_is_help=True,
+)
+
+
+@app.callback()
+def main():
+    pass
+
+    
+# def app():
+#     print("CLI entry")
